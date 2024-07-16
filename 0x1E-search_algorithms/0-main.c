eebd3272c4092fa23a27ff78217420ca5645a93a
radwaneinbox <redzmourine671@gmail.com>
2024-07-16 23:53:46 +0200

    algorithms

---
+++ 0x1E-search_algorithms/0-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+int main(void)
+{
+    int array[] = {
+        10, 1, 42, 3, 4, 42, 6, 7, -1, 9
+    };
+    size_t size = sizeof(array) / sizeof(array[0]);
+
+
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,2 @@
+project
+0x1E-search_algorithms
